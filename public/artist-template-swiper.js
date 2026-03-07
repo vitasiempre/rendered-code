797076ffc2d8718ac40b37a4c36fbaf499131078
“vitasiempre” <wavelolmu@gmail.com>
2026-03-07 17:04:36 +0400

    trying to fix delivery

--- public/artist-template-swiper.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("I am connected!");
-
-    $(".slider-main-component").each(function() {
-        const Swiper = new Swiper($(this).find(".swiper")[0], {
-
-        })
-    })
-})
