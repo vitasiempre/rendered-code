c88589a1f315d697cd199be83fc17b72016f7c87
“vitasiempre” <wavelolmu@gmail.com>
2026-04-15 16:42:39 +0400

    added event cards swiper

---
+++ event-cards-swiper.js
@@ -0,0 +1,43 @@
+$(".events-slider").each(function index() {
+  const swiperEl = $(this).find(".swiper")[0];
+
+  let swiper = new Swiper(swiperEl, {
+    slidesPerView: "auto",
+    speed: 500,
+    mousewheel: {
+      forceToAxis: true,
+    },
+    keyboard: true,
+    freeMode: true,
+    spaceBetween: 10,
+    breakpoints: {
+      481: {
+        spaceBetween: 30,
+      },
+    },
+  });
+
+  const s = document.querySelector(".swiper").swiper;
+  console.log("container", s.width);
+  console.log("virtualSize", s.virtualSize);
+  console.log("isLocked", s.isLocked);
+
+  function refreshSwiper() {
+    swiper.updateSize();
+    swiper.updateSlides();
+    swiper.updateProgress();
+    swiper.updateSlidesClasses();
+    swiper.navigation.update();
+  }
+
+  // after full page load
+  window.addEventListener("load", refreshSwiper);
+
+  // after each image inside this slider loads
+  $(this)
+    .find("img")
+    .each(function () {
+      if (this.complete) return;
+      this.addEventListener("load", refreshSwiper);
+    });
+});
